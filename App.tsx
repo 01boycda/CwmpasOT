5e10034bbe33c00716cb76d5220373b8558a89bf
boy_c <boy_c@hotmail.co.uk>
2025-04-30 16:44:44 +0100

    .env fix

--- App.tsx
+++ App.tsx
@@ -1,17 +1,18 @@
 import { StatusBar } from "expo-status-bar";
-import React, { Suspense, useEffect, useState } from "react";
-import { ActivityIndicator, Alert, Platform, SafeAreaView, StyleSheet, View } from "react-native";
+import React, { useEffect } from "react";
+import { Alert, Platform, SafeAreaView, StyleSheet, View } from "react-native";
 import Navigator from "./Navigator";
 
 import { useFonts } from "expo-font";
 import * as SplashScreen from "expo-splash-screen";
 import COLOURS from "./constants/colours";
 
-import { SQLiteProvider, openDatabaseSync } from "expo-sqlite";
-
 // RevenueCat
 import Purchases, { LOG_LEVEL } from "react-native-purchases";
 Purchases.setLogLevel(Purchases.LOG_LEVEL.VERBOSE);
+
+const REACT_APP_RC_IOS = "appl_qmbICYZdvDRdRfteThzvGEFdpoZ";
+const REACT_APP_RC_ANDROID = "goog_fbifMoYvPrHIDXGzBheNCrHEgmn";
 
 const App = () => {
   const [loaded, error] = useFonts({
@@ -28,22 +29,22 @@ const App = () => {
   useEffect(() => {
     Purchases.setLogLevel(LOG_LEVEL.DEBUG);
     if (Platform.OS === "ios") {
-      if (!process.env.EXPO_PUBLIC_RC_IOS) {
+      if (!REACT_APP_RC_IOS) {
         Alert.alert(
           "RC Error Config",
           "RevenueCat API key for ios not provided"
         );
       } else {
-        Purchases.configure({ apiKey: process.env.EXPO_PUBLIC_RC_IOS });
+        Purchases.configure({ apiKey: REACT_APP_RC_IOS });
       }
     } else if (Platform.OS === "android") {
-      if (!process.env.EXPO_PUBLIC_RC_ANDROID) {
+      if (!REACT_APP_RC_ANDROID) {
         Alert.alert(
           "RC Error Config",
           "RevenueCat API key for android not provided"
         );
       } else {
-        Purchases.configure({ apiKey: process.env.EXPO_PUBLIC_RC_ANDROID });
+        Purchases.configure({ apiKey: REACT_APP_RC_ANDROID });
       }
     }
   }, [])
